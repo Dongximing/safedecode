0fab89835125a36fbfc4b17d91420545e88ec1c7
Ximing Dong <ximing@mac.home>
2025-06-29 19:25:07 -0400

    add exp

--- exp/run.sh
+++ exp/run.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-python defense.py --model_name llama2 --attacker GCG --defender PPL --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  AutoDAN --defender PPL --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  PAIR --defender PPL --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  DeepInception --defender PPL --disable_GPT_judge
+python defense.py --model_name vicuna --attacker GCG --defender PPL --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  AutoDAN --defender PPL --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  PAIR --defender PPL --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  DeepInception --defender PPL --disable_GPT_judge
 
-python defense.py --model_name llama2 --attacker GCG --defender  Self-Exam --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  AutoDAN --defender  Self-Exam --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  PAIR --defender  Self-Exam --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  DeepInception --defender  Self-Exam --disable_GPT_judge
+python defense.py --model_name vicuna --attacker GCG --defender  Self-Exam --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  AutoDAN --defender  Self-Exam --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  PAIR --defender  Self-Exam --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  DeepInception --defender  Self-Exam --disable_GPT_judge
 
-python defense.py --model_name llama2 --attacker GCG --defender Paraphrase --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  AutoDAN --defender Paraphrase --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  PAIR --defender Paraphrase --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  DeepInception --defender Paraphrase --disable_GPT_judge
+python defense.py --model_name vicuna --attacker GCG --defender Paraphrase --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  AutoDAN --defender Paraphrase --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  PAIR --defender Paraphrase --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  DeepInception --defender Paraphrase --disable_GPT_judge
 
-python defense.py --model_name llama2 --attacker GCG --defender Retokenization --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  AutoDAN --defender Retokenization --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  PAIR --defender Retokenization --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  DeepInception --defender Retokenization --disable_GPT_judge
+python defense.py --model_name vicuna --attacker GCG --defender Retokenization --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  AutoDAN --defender Retokenization --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  PAIR --defender Retokenization --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  DeepInception --defender Retokenization --disable_GPT_judge
 
-python defense.py --model_name llama2 --attacker GCG --defender Self-Reminder --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  AutoDAN --defender Self-Reminder --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  PAIR --defender Self-Reminder --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  DeepInception --defender Self-Reminder --disable_GPT_judge
+python defense.py --model_name vicuna --attacker GCG --defender Self-Reminder --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  AutoDAN --defender Self-Reminder --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  PAIR --defender Self-Reminder --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  DeepInception --defender Self-Reminder --disable_GPT_judge
 
 
-python defense.py --model_name llama2 --attacker GCG --defender ICD --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  AutoDAN --defender ICD --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  PAIR --defender ICD --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  DeepInception --defender ICD --disable_GPT_judge
+python defense.py --model_name vicuna --attacker GCG --defender ICD --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  AutoDAN --defender ICD --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  PAIR --defender ICD --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  DeepInception --defender ICD --disable_GPT_judge
 
-python defense.py --model_name llama2 --attacker GCG --defender SafeDecoding --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  AutoDAN --defender SafeDecoding --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  PAIR --defender SafeDecoding --disable_GPT_judge
-python defense.py --model_name llama2 --attacker  DeepInception --defender SafeDecoding  --disable_GPT_judge
+python defense.py --model_name vicuna --attacker GCG --defender SafeDecoding --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  AutoDAN --defender SafeDecoding --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  PAIR --defender SafeDecoding --disable_GPT_judge
+python defense.py --model_name vicuna --attacker  DeepInception --defender SafeDecoding  --disable_GPT_judge
 
 
 
